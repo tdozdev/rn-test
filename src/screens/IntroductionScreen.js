fda76e1dc74dd2785d31947a8ecd7ab1c4f929c9
KYG <ygcarrot@gmail.com>
2024-04-01 18:13:49 +0900

    프로필화면 구현

--- src/screens/IntroductionScreen.js
+++ src/screens/IntroductionScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 
 function IntroductionScreen() {
   return (
     <View style={styles.container}>
-      <Text>자기소개</Text>
+      <Image
+        source={require('../../assets/profile.jpeg')}
+        style={styles.profilePic}
+      />
+      <Text style={styles.name}>김영근</Text>
+      <Text style={styles.contact}>휴대폰 번호: 010-9458-8523</Text>
+      <Text style={styles.contact}>이메일: ygcarrot@gmail.com</Text>
+      <TouchableOpacity onPress={() => Linking.openURL('https://github.com/tdozdev')}>
+        <Text style={styles.githubLink}>GitHub: tdozdev</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -12,8 +21,26 @@ function IntroductionScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  profilePic: {
+    width: 150,
+    height: 150,
+    borderRadius: 75, // 원형 이미지를 만들기 위해
+  },
+  name: {
+    fontSize: 24,
+    marginVertical: 8,
+  },
+  contact: {
+    fontSize: 16,
+    marginVertical: 4,
+  },
+  githubLink: {
+    fontSize: 16,
+    color: 'blue',
+    marginVertical: 8,
   },
 });
 
