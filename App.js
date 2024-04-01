fbc9a46ad385b0dfa17679f2508d9bba32cac37f
KYG <ygcarrot@gmail.com>
2024-04-01 18:03:17 +0900

    하단 탭에 아이콘 적용

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import IntroductionScreen from './src/screens/IntroductionScreen';
 import ProjectsScreen from './src/screens/ProjectsScreen';
 import SkillsScreen from './src/screens/SkillsScreen';
@@ -11,7 +12,24 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === 'Introduction') {
+          iconName = focused ? 'person' : 'person-outline';
+        } else if (route.name === 'Projects') {
+          iconName = focused ? 'briefcase' : 'briefcase-outline';
+        } else if (route.name === 'Skills') {
+          iconName = focused ? 'build' : 'build-outline';
+        } else if (route.name === 'Others') {
+          iconName = focused ? 'settings' : 'settings-outline';
+        }
+
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}>
       <Tab.Screen name="Introduction" component={IntroductionScreen} />
       <Tab.Screen name="Projects" component={ProjectsScreen} />
       <Tab.Screen name="Skills" component={SkillsScreen} />
